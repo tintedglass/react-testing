2ef62b6f5c1be34b1b718b0a0d0ffee290f588f1
Katherine Coley <savi.tintedglass@gmail.com>
2016-12-13 10:21:02 -0600

    Get messages from database

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 import Message from './Message';
+// Import all firebase modules
 import * as firebase from 'firebase';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
+            // Set initial state of messages array to blank
             messages: []
         };
     }
 
     componentDidMount() {
+        // Connect to database
         let database = firebase.database().ref('messages');
+        // Remove all callbacks from objects
         database.off();
 
+        // setMessage function, @param message: a message object
         let setMessage = function(message) {
+            // Gets the values of the message object (a key and text in this case)
             let messageValues = message.val();
+            // Run the createMessage function to create a Message component using this messages key and text
             this.createMessage(message.key, messageValues.text);
+            // Bind this to this function
             }.bind(this);
 
+        // Grab the last 12 rows from the database, on child added, run the setMessage function above
         database.limitToLast(12).on('child_added', setMessage);
-        database.limitToLast(12).on('child_changed', setMessage);
+
+        // Grab the last 12 rows from the database and on change, do a thing
+        //database.limitToLast(12).on('child_changed', doathing);
     }
 
+    // Creates a message @param key: the key of the message in database @param text: the text of the message
     createMessage(key, text){
+        // Create a new Message component with key and text df
         let thisMessage = <Message key={key} text={text} />;
+
+        // Add this new Message component to this state messages array
         this.setState({
             messages: this.state.messages.concat(thisMessage)
         })
     }
 
+    // Return the messages array of Message components
     render() {
         return (<div>{this.state.messages}</div>);
     }
--- src/Message.js
+++ src/Message.js
@@ -5,10 +5,26 @@ import React from 'react';
 
 // New class that extends react
 class Message extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            key: this.props.key,
+            text: this.props.text
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            text: "Booger"
+        })
+    }
+
 
     // Render the component
     render(){
-        return <div className="messageContainer"><b>Message {this.props.text}</b></div>
+        return <div className="messageContainer"><b>Message {this.state.text}</b> <button onClick={this.handleClick}>Edit</button></div>
     }
 }
 
