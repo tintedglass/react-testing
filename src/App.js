e1cc72209ee7437c685fda4033910ca03f8fb56d
Katherine Coley <savi.tintedglass@gmail.com>
2016-12-14 09:55:15 -0600

    Get messages from database and edit them with set text

--- src/App.js
+++ src/App.js
@@ -15,21 +15,20 @@ class App extends Component {
 
     componentDidMount() {
         // Connect to database
-        let database = firebase.database().ref('messages');
+        const database = firebase.database().ref('messages');
         // Remove all callbacks from objects
         database.off();
 
         // setMessage function, @param message: a message object
-        let setMessage = function(message) {
+        const setMessage = (message) => {
             // Gets the values of the message object (a key and text in this case)
             let messageValues = message.val();
             // Run the createMessage function to create a Message component using this messages key and text
             this.createMessage(message.key, messageValues.text);
-            // Bind this to this function
-            }.bind(this);
+            };
 
         // Grab the last 12 rows from the database, on child added, run the setMessage function above
-        database.limitToLast(12).on('child_added', setMessage);
+        database.on('child_added', setMessage);
 
         // Grab the last 12 rows from the database and on change, do a thing
         //database.limitToLast(12).on('child_changed', doathing);
--- src/Message.js
+++ src/Message.js
@@ -8,30 +8,72 @@ import * as firebase from 'firebase';
 class Message extends React.Component {
     constructor(props) {
         super(props);
+        // Set initial state of this message, id is the key from firebase, text is the text from firebase
         this.state = {
             id: this.props.id,
-            text: this.props.text
+            text: this.props.text,
+            editing: false
         };
 
-        this.handleClick = this.handleClick.bind(this);
+        // Bind methods to this instance of Message
+        this.editMessage = this.editMessage.bind(this);
+        this.changeMessage = this.changeMessage.bind(this);
+        this.edit = this.edit.bind(this);
+        this.renderForm = this.renderForm.bind(this);
+        this.renderMessage = this.renderMessage.bind(this);
     }
 
-    handleClick() {
+    // When the edit button is clicked set the editing state to true
+    edit() {
         this.setState({
-            text: "Booger"
-        })
+            editing: true
+        });
+    }
 
+    // Render the message with an edit form
+    renderForm() {
+        return (
+            <div><input type="text" defaultValue={this.state.text} onChange={this.changeMessage} /> <button onClick={this.editMessage}>Save</button></div>
+        )
+    }
+
+    // Render the message with an edit button
+    renderMessage(){
+        return (
+            <span>{this.state.text}
+            <button onClick={this.edit}>Edit</button></span>
+        )
+    }
+
+    // When the edit form changes change the text state to value of form input
+    changeMessage(e) {
+        this.setState({
+            text: e.target.value,
+        });
+    }
+
+    // When the save button is pressed
+    editMessage() {
+        // Set editing to false
+        this.setState({
+          editing:false
+        });
+
+        // Save current state of text to the database
         let database = firebase.database().ref('messages').child(this.state.id).child('text');
-        //let thisMessage = database.child(this.state.id);
-
-        database.set("Booger");
+        database.set(this.state.text);
 
     }
 
 
-    // Render the component
-    render(){
-        return <div className="messageContainer"><b>Message {this.state.text}</b> <button onClick={this.handleClick}>Edit</button></div>
+    // Render the form if state is editing, otherwise render the message
+    render() {
+        return(
+            <div className="messageContainer">
+                {(this.state.editing) ? this.renderForm()
+                                    : this.renderMessage()}
+            </div>)
+
     }
 }
 
